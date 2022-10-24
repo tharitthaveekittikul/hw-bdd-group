091d1871fe3e35c23d72d1b335814dd8b39e98a4
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2022-10-24 20:15:42 +0700

    fork git

---
+++ rottenpotatoes/features/support/paths.rb
@@ -0,0 +1,31 @@
+# TL;DR: YOU SHOULD DELETE THIS FILE
+#
+# This file is used by web_steps.rb, which you should also delete
+#
+# You have been warned
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
+  def path_to(page_name)
+    case page_name
+
+    when /^the movies page$/ then '/movies'
+      
+      # Here is an example that uses value from the Regexp:
+      #
+      #   when /^the details page for movie "(.*)"$/ do |movie_name|
+      #     movie_path(Movie.find_by(:name => movie_name))
+      # end
+
+    else
+      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in #{__FILE__}"
+    end
+  end
+end
+
+World(NavigationHelpers)
