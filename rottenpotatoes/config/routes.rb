a85d65ce4ae36aa6c4003777083c8e5437d5d603
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2022-10-29 15:56:40 +0700

    add sso omniauth google

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,7 +1,21 @@
 Rottenpotatoes::Application.routes.draw do
+  root :to => redirect('/movies')
+  get 'pages/home'
+
+  devise_for :users, controllers:{
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+
+  # root 'pages#home'
+  # devise_for :users, controllers: {
+  #   omniauth_callbacks: 'users/omniauth_callbacks',
+  #   sessions: 'users/sessions',
+  #   registrations: 'users/registrations'
+  # }
   
   post '/movies/search_tmdb' => 'movies#search_tmdb', :as => 'search_tmdb'
 end
