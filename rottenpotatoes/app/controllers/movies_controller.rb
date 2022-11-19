2440650f77089536983f62715d82cb60be993cc6
Kornsakon-s63 <s6301012620014@email.kmutnb.ac.th>
2022-11-20 00:36:29 +0700

    Refactoring code Smell

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,12 +1,15 @@
+#MoviesController manage for CRUD operations
 class MoviesController < ApplicationController
 
+  #display movie on first page
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
+
+  def sort_data
     sort = params[:sort] || session[:sort]
     case sort
     when 'title'
@@ -14,35 +17,47 @@ class MoviesController < ApplicationController
     when 'release_date'
       ordering,@date_header = {:release_date => :asc}, 'bg-warning hilite'
     end
+    return sort, ordering
+  end
+
+  def return_sorted_data
+    sort = sort_data[0]
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    resume(sort_data[1])
+  end
+
+  def resume(order_sort)
+    @movies = Movie.where(rating: @selected_ratings.keys).order(order_sort)
+  end
+
+  #retrieve movie from database and return to index page
+  def index
     @all_ratings = Movie.all_ratings
     @selected_ratings = params[:ratings] || session[:ratings] || {}
 
     if @selected_ratings == {}
       @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
     end
-
-    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+    return_sorted_data
   end
 
-  def new
-    # default: render 'new' template
-  end
-
+  #create new movie
   def create
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
+  #find all movies from ID
   def edit
     @movie = Movie.find params[:id]
   end
 
+  #update movie
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(params[:movie])
@@ -50,6 +65,7 @@ class MoviesController < ApplicationController
     redirect_to movie_path(@movie)
   end
 
+  #destroy movie
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
@@ -57,32 +73,35 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  def search_tmdb
-    api_key = "d7c385ccb2f4441630477b180f3861e3"
-    url = URI("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{params[:search_terms]}&page=1")
-    response = Net::HTTP.get_response(url)
+  def fetch_from_tmdb
+    response = Net::HTTP.get_response(URI("https://api.themoviedb.org/3/search/movie?api_key=d7c385ccb2f4441630477b180f3861e3&query=#{params[:search_terms]}&page=1"))
     if response.is_a?(Net::HTTPSuccess)
-      data = JSON.parse(response.body)
-      # puts "THIS ---------------------- #{data["results"]}"
-      if data["results"] == []
-        flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
-        redirect_to movies_path   
-      else
-        title_name = data["results"][0]["title"]
-        release_date = data["results"][0]["release_date"]
-        adult = data["results"][0]["adult"]
-        overview = data["results"][0]["overview"]
-        @movies = {
-          "title" => "#{title_name}",
-          "release_date" => "#{release_date}",
-          "adult" => "#{adult}",
-          "overview" => "#{overview}"
-        }
-        # puts "THIS -------------------- #{@movies}"
-        redirect_to new_movie_path(@movies)        
-      end
-      
-    end    
+      retrieve = JSON.parse(response.body)
+      data = retrieve["results"][0]
+      return data
+    else 
+      return nil
+    end
+
+  end
+
+  #search movie from TMDb and return to create page
+  def search_tmdb
+    data = fetch_from_tmdb
+    if data
+      @movies = {
+        "title" => "#{data["title"]}",
+        "release_date" => "#{data["release_date"]}",
+        "adult" => "#{data["adult"]}",
+        "overview" => "#{data["overview"]}"
+      }
+      redirect_to new_movie_path(@movies)
+    else
+      flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
+      redirect_to movies_path
+    end
+
+        
   end
 
 end
